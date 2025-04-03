13a8e66da67fccd8a62432c8f0aa7c8b62eeea67
R-Ying <j20010113@gmail.com>
2025-04-03 22:22:03 +0800

    feat: 新增前端和後端的基本檔案結構，包括配置文件、樣式、元件和視圖

---
+++ social-media-platform-backend/src/main/java/io/github/rying/socialmedia/backend/SocialMediaPlatformBackendApplication.java
@@ -0,0 +1,13 @@
+package io.github.rying.socialmedia.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocialMediaPlatformBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocialMediaPlatformBackendApplication.class, args);
+	}
+
+}
---
+++ social-media-platform-backend/src/test/java/io/github/rying/socialmedia/backend/SocialMediaPlatformBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.rying.socialmedia.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SocialMediaPlatformBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
